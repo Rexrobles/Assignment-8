146d72133257331b2180817c5d9b0c43133f4e89
Rex-Imman Robles <reximman03@gmail.com>
2021-12-06 21:50:52 +0800

    finish coding

--- lottery.py
+++ lottery.py
@@ -40,3 +40,41 @@ def get3WinningNum():
     W_Num1, W_num2, W_Num3 = random.sample(range(0, 9), 4)
     Winning_list = [W_Num1, W_num2, W_Num3]
     return Winning_list
+
+# Win or Lose
+def checkWinLose(list1, list2):
+    if sorted(list1) == sorted(list2):
+        print("Winner!")
+    else: 
+        print("You loss!")
+        
+# start the game
+def play():
+    print("Welcome to the Lottery!")
+    # asks user 3 numbers and generates 3 winning numbers
+    inputNums = ask()
+    winningNums = get3WinningNum()
+    # dislay user input and winning numbers
+    print(f"Your guesses: {inputNums} Winning numbers: {winningNums}")
+    # Show Result
+    print("RESULT") 
+    checkWinLose(inputNums, winningNums)
+   
+# asks user to play again
+def playAgain():
+    while True:
+        yn = input("Try again? y/n: ")
+        yn = yn.lower()
+        if yn == 'y':
+            print("n")
+            play()
+        elif yn == 'n':
+            print("Bye player!")
+            break
+        else:
+            print("n")
+            print("Please enter y if you want to play again, or enter n to exit the game.")
+
+# main
+play()
+playAgain()
