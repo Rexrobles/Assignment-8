6463c1d8ade2f9271b65929f0edb9d6344b20a4f
Rex-Imman Robles <reximman03@gmail.com>
2021-12-06 20:36:27 +0800

    ask input from user

--- lottery.py
+++ lottery.py
@@ -9,3 +9,28 @@
 # Greetings
 print("Hello! This Program will ask you a three winning number from (0-9)")
 
+import random
+
+#ask input from the user
+def ask():
+    while True:
+        try: 
+            FirstNum = int(input("Enter first number (0-9): "))
+            if FirstNum not in range(0,9):
+                print("Invalid. Input is out of range (0-9)") 
+                continue
+            SecondNum = int(input("Enter second number (0-9): "))
+            if SecondNum not in range(0,9):
+                print("Invalid. Input is out of range (0-9)") 
+                continue
+            ThridNum = int(input("Enter third number (0-9): "))
+            if ThridNum not in range(0,9):
+                print("Invalid. Input is out of range (0-9)") 
+                continue
+        except ValueError:
+            print("Invalid. Please enter a number.")
+            continue
+        else:
+            break
+    List = [FirstNum, SecondNum, ThridNum]
+    return List
