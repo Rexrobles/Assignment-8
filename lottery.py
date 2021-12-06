a306ef61548a0ddcd481b73dbcbf4e9bde6aa1de
Rex-Imman Robles <reximman03@gmail.com>
2021-12-06 19:29:04 +0800

    greetings

---
+++ lottery.py
@@ -0,0 +1,11 @@
+# Create a program that ask 3 numbers (0-9) from the user.
+# Generate 3 random winning numbers (0-9)
+# Display “Winner” if all 3 input numbers matched the generated numbers
+# Display ”You loss” if not
+# Display ”Try again y/n” after each game
+# If the user enter “y” the user will play again
+# if “n” the program will exit.
+
+# Greetings
+print("Hello! This Program will ask you a three winning number from (0-9)")
+
