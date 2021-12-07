2c099786dea731bf41b7ba5f9f5689dcd2777ce9
Rex-Imman Robles <reximman03@gmail.com>
2021-12-07 18:37:58 +0800

    greeting to the user

---
+++ Guess_The_Number.py
@@ -0,0 +1,9 @@
+# Program 2: Guess the number
+# Generate 1 random number (0-100)
+# Ask the user to guess the number
+# Display “Greater than” if the inputed number is greater than the random number
+# Display “Less than” if the inputed number is less than the random number
+# Repeat asking the user until the random number has been guessed correctly.
+
+# greetings to the user
+print ("This program will generate a random number from 0 to 100.")
